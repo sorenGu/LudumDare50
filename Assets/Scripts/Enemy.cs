bee1c1b8767915a9c8fcae32b6dcacb85aef22e4
sorenGu <soeren.gugel@gmail.com>
2022-04-02 17:20:55 +0200

    Update 13

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    GameController gameController;
+    public List<Transform> foods;
+
+    float eatDistance = 2f;
+
+    private void OnEnable() {
+        gameController = transform.parent.GetComponent<GameController>();
+    }
+
+    void Update() {
+        CheckDistance();
+        transform.Translate(Vector3.forward * Time.deltaTime * (float)(gameController.gameSpeed * 0.05));
+    }
+
+    private void CheckDistance() {
+        foreach(Transform food in foods) {
+            float distance = (transform.position - food.position).sqrMagnitude;
+            if (distance < eatDistance) {
+                // Todo gameOver
+                Debug.Log("AaA!!!");
+            } else if (distance < eatDistance * 4) {
+                // TODO spookier sounds and new animation
+            } else {
+                // TODO rotate to normal (delegate)
+            }
+        }
+    }
+}
